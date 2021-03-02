8299bae25a496a44243c19ae1477400e32066104
IGP <pyltsov@gmail.com>
2021-03-02 19:06:50 +0100

    Практическое задание 29.3.4

--- Prj26_3FileManager/ViewController.swift
+++ Prj26_3FileManager/ViewController.swift
@@ -13,9 +13,11 @@ class ViewController: UIViewController {
     
     let fileManager = FileManager()
     let tempDirectory = NSTemporaryDirectory()
+    //let docsDirectory =
     let fileName = "customFile.txt"
+    let folderName = "CustomFolder"
     let myFileName = "otherfile.txt"
-    
+    var folderPath = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +53,7 @@ class ViewController: UIViewController {
         
     }
     
+    // метод был изменен так чтобы выводил список файоа из каталога
     func validateCatalog() -> String? {
         do {
             let objectsInCatalog = try fileManager.contentsOfDirectory(atPath: tempDirectory)
@@ -67,7 +70,7 @@ class ViewController: UIViewController {
                 return nil
                 }
             } catch {return nil}
-        //catch let error as NSError {
+        //catch let error as NSError {  
           //  addressLabel.text = "Error  - \(error)"
         //}
 }
@@ -75,6 +78,77 @@ class ViewController: UIViewController {
     @IBAction func checkTmp(_ sender: UIButton) {
         let fileCatalog = validateCatalog() ?? "Nothing"
         addressLabel.text = fileCatalog
+    }
+    
+    @IBAction func createFolderBtnPressed(_ sender: UIButton) {
+        let docsFolderPath = NSURL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])
+        let logPath = docsFolderPath.appendingPathComponent(folderName)
+        guard let unwrLogPath = logPath else {return}
+        do {
+            try FileManager.default.createDirectory(atPath: unwrLogPath.path, withIntermediateDirectories: true, attributes: nil)
+            addressLabel.text = "\(unwrLogPath)"
+        } catch let error as NSError {
+            addressLabel.text = "Can't create a directory, \(error)"
+        }
+    }
+    
+    @IBAction func checkFolder(_ sender: UIButton) {
+        let directories : [String] = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true)
+        if directories.count > 0 {
+            let directory  = directories[0] //дирктория с документами
+            folderPath = directory.appendingFormat("/" + fileName)
+            print("Local path = \(folderPath)")
+        } else {
+            print("Could not fond local directory for this folder")
+        }
+        // проверка, есть ли файл
+        if fileManager.fileExists(atPath: folderPath) {
+            addressLabel.text = "Folder exists  - \(folderPath)"
+        } else {
+            addressLabel.text = "Folder does not exists"
+        }
+    }
+    
+    
+    @IBAction func readFile(_ sender: UIButton) {
+        let directoryWithFile = myFileName
+        let path = (tempDirectory as NSString).appendingPathComponent(directoryWithFile)
+        do {
+           let contentsOfFile = try NSString(contentsOfFile: path, encoding: String.Encoding.utf8.rawValue)
+            addressLabel.text = String(contentsOfFile)
+        } catch let error as NSError {
+            addressLabel.text = "Иррор \(error)"
+        }
+    }
+    
+    @IBAction func writeToFile(_ sender: UIButton) {
+        let someText = "Bonjour, fichier !!! Pouvez-vous me lire?"
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let fileURL = dir.appendingPathComponent(fileName)
+            do {
+                try someText.write(to: fileURL, atomically: false, encoding: .utf8)
+                addressLabel.text = "'\(someText)' added to '\(fileName)'"
+            } catch {
+                addressLabel.text = "Unable to write"
+            }
+        }
+    }
+    @IBAction func readFromWritedFile(_ sender: UIButton) {
+        
+        //let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first
+
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        let fullPath = documentDirectory?.appendingPathComponent(fileName)
+        do {
+            let contentsOfFile = try NSString(contentsOfFile: fullPath?.path ?? "", encoding: String.Encoding.utf8.rawValue)
+            addressLabel.text = contentsOfFile as String
+        } catch let error as NSError {
+            addressLabel.text = "Error  \(error)"
+        }
+        
+        
+        
+        
     }
     
 }
